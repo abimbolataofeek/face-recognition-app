40553466728fae47e4601a13d53df8b6aac62ad1
abimbolataofeek <abimbolaadewale9999@gmail.com>
2022-01-15 06:46:04 +0100

    initial commit navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navigation = () =>{
+    return(
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className="f3 link dim black underline pa3 pointeer">Sign Out</p>
+
+        </nav>
+    )
+
+}
+export default Navigation;
