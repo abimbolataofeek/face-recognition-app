fcde4dc7362243e99937405eff1dcce48151d387
abimbolataofeek <abimbolaadewale9999@gmail.com>
2022-01-15 21:42:44 +0100

    add clarifai api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Particles from "react-tsparticles";
 import './App.css';
 import tachyons from 'tachyons';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import { Component } from "react/cjs/react.production.min";
 
-
+const app = new Clarifai.App({
+  apikey: '4775b1829ea647359c8c79cf321efa02e'
+});
 
 const particlesInit = (main) => {
   console.log(main);
@@ -24,15 +28,31 @@ class App extends Component{
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input:event.target.value});
   }
 
   onButtonSubmit = () => {
-  console.log('click');
+  this.setState({imageUrl: this.state.input});
+  app.model.predict(
+    Clarifai.FACE_DETECT_MODEL,
+    this.state.input)
+    .then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        //err
+        
+      }
+    )
+
+
+
 }
 
   render() {
@@ -45,6 +65,8 @@ class App extends Component{
         <ImageLinkForm
          onInputChange={this.onInputChange} 
         onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+
 
         <Particles className="particles"
 
---
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FaceRecognition = ({imageUrl}) =>{
+    return(
+     <div className="center ma  ">
+         <div className="absolute mt2">
+         <img alt="image" src="{imageUrl} width= '500px' height='auto'" />
+         </div>
+        
+
+     </div>
+
+
+    )
+
+}
+export default FaceRecognition;
